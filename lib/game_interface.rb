cb8e5899b64a23dd05a0805ffbe15d74097075df
Ben Smith <ben@smithsrus.com>
2021-06-09 16:47:55 -0400

    game interface sends private result to the player who took the turn

--- lib/game_interface.rb
+++ lib/game_interface.rb
@@ -14,4 +14,10 @@ class GameInterface
     end
     client_players
   end
+
+  def describe_result_to_clients(result)
+    turn_player_client_hash = clients_to_players.select { |client_player_hash| client_player_hash[:player] == result.turn_player}
+    turn_client = turn_player_client_hash.pop[:client]
+    turn_client.puts result.private_description
+  end
 end
--- spec/game_interface_spec.rb
+++ spec/game_interface_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/game_interface'
+require_relative '../lib/turn_result'
 
 describe 'GameInterface' do
   context '#initialize' do
@@ -26,6 +27,39 @@ describe 'GameInterface' do
       expect(clients_to_players[1][:player]).to_not be_nil
     end
   end
+
+  context 'describe_result_to_clients' do
+    let!(:server) { TCPServer.new(3336) }
+    let!(:clients) { [] }
+
+    def create_client_and_socket_array(count)
+      client_socket_array = []
+      count.times do
+        client_socket_array << create_and_accept_client
+      end
+      client_socket_array
+    end
+
+    def create_and_accept_client
+      client = TCPSocket.new('localhost', 3336)
+      clients << client
+      socket = server.accept
+      {client: client, socket: socket}
+    end
+
+    after(:each) do
+      server.close
+      clients.each do |client|
+        client.close if client
+      end
+    end
+
+    it 'sends the private result description to the player who took the turn' do
+      clients_and_sockets = create_client_and_socket_array(3)
+      interface = GameInterface.new([clients_and_sockets[0][:socket], clients_and_sockets[1][:socket], clients_and_sockets[2][:socket]])
+      result = TurnResult.new(interface.clients_to_players[0][:player])
+      interface.describe_result_to_clients(result)
+      expect(clients_and_sockets[0][:client].gets.chomp).to eq result.private_description
     end
   end
 end
