de51792974f1ecab8956659bd05496cfe19a655e
Ben Smith <ben@smithsrus.com>
2021-06-08 20:49:34 -0400

    add start and stop methods

--- lib/go_fish_server.rb
+++ lib/go_fish_server.rb
@@ -1,7 +1,17 @@
 require 'socket'
 
 class GoFishServer
+  attr_reader :server
+
   def port_number
     3336
+  end
+
+  def start
+    @server = TCPServer.new(port_number)
+  end
+
+  def stop
+    server.close if server
   end
 end
--- spec/go_fish_server_spec.rb
+++ spec/go_fish_server_spec.rb
@@ -3,6 +3,14 @@ require_relative '../lib/go_fish_server.rb'
 describe 'GoFishServer' do
   let(:server) { GoFishServer.new }
 
+  before(:each) do
+    server.start
+  end
+
+  after(:each) do
+    server.stop
+  end
+
   it 'returns the port number' do
     expect(server.port_number).to eq 3336
   end
