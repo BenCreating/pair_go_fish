b35f8acf979c687fb66756930ae9533d79cf3176
Ben Smith <ben@smithsrus.com>
2021-06-09 10:55:00 -0400

    client reads messages from server

--- lib/go_fish_client.rb
+++ lib/go_fish_client.rb
@@ -10,4 +10,11 @@ class GoFishClient
   def close
     socket.close
   end
+
+  def read_message
+    sleep(0.1)
+    socket.read_nonblock(1000).chomp
+  rescue IO::WaitReadable
+    'No message'
+  end
 end
