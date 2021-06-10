6c1639ff77ba6bccfb8262cd3a00188449c4968b
Ben Smith <ben@smithsrus.com>
2021-06-09 20:06:36 -0400

    refactor GameInterface

--- lib/game_interface.rb
+++ lib/game_interface.rb
@@ -1,38 +1,25 @@
 require_relative 'player'
+require_relative 'player_interface'
+
+require 'pry'
 
 class GameInterface
-  attr_reader :clients_to_players
+  attr_reader :player_interfaces
 
   def initialize(clients)
-    @clients_to_players = associate_clients_with_players(clients)
-  end
-
-  def associate_clients_with_players(clients)
-    client_players = []
+    @player_interfaces = []
     clients.each do |client|
-      client_players << {client: client, player: Player.new}
+      player_interfaces << PlayerInterface.new(client)
     end
-    client_players
   end
 
   def describe_result_to_clients(result)
-    describe_to_turn_player(result)
-    describe_to_other_players(result)
-  end
-
-  private
-  def describe_to_turn_player(result)
-    turn_player_client_hash = clients_to_players.select { |client_player_hash| client_player_hash[:player] == result.turn_player}
-    turn_client = turn_player_client_hash.pop[:client]
-    turn_client.puts result.private_description
-  end
-
-  def describe_to_other_players(result)
-    turn_player_client_hashes = clients_to_players.select { |client_player_hash| client_player_hash[:player] != result.turn_player}
-    turn_clients = []
-    turn_player_client_hashes.each do |hash|
-      turn_client = hash[:client]
-      turn_client.puts result.public_description
+    player_interfaces.each do |interface|
+      if interface.player == result.turn_player
+        interface.send_message_to_client(result.private_description)
+      else
+        interface.send_message_to_client(result.public_description)
+      end
     end
   end
 end
--- spec/game_interface_spec.rb
+++ spec/game_interface_spec.rb
@@ -1,30 +1,17 @@
 require_relative '../lib/game_interface'
+require_relative '../lib/player_interface'
 require_relative '../lib/turn_result'
 
 describe 'GameInterface' do
   context '#initialize' do
-    it 'takes client array as input and associates them with players in an array of hashes' do
+    it 'takes client array as input and creates a player interface for each' do
       client1 = 'client1'
       client2 = 'client2'
       interface = GameInterface.new([client1, client2])
-      expect(interface.clients_to_players[0][:client]).to eq client1
-      expect(interface.clients_to_players[0][:player]).to_not be_nil
-      expect(interface.clients_to_players[1][:client]).to eq client2
-      expect(interface.clients_to_players[1][:player]).to_not be_nil
-    end
-  end
-
-  context '#associate_clients_with_players' do
-    let(:client1) { 'client1' }
-    let(:client2) { 'client1' }
-
-    it 'returns an array of hashes that associate clients and players' do
-      interface = GameInterface.new([])
-      clients_to_players = interface.associate_clients_with_players([client1, client2])
-      expect(clients_to_players[0][:client]).to eq client1
-      expect(clients_to_players[0][:player]).to_not be_nil
-      expect(clients_to_players[1][:client]).to eq client2
-      expect(clients_to_players[1][:player]).to_not be_nil
+      expect(interface.player_interfaces[0].client).to eq client1
+      expect(interface.player_interfaces[0].player).to_not be_nil
+      expect(interface.player_interfaces[1].client).to eq client2
+      expect(interface.player_interfaces[1].player).to_not be_nil
     end
   end
 
@@ -32,7 +19,7 @@ describe 'GameInterface' do
     let!(:server) { TCPServer.new(3336) }
     let!(:clients) { [] }
 
-    def create_client_and_socket_array(count)
+    def create_clients_and_sockets(count)
       client_socket_array = []
       count.times do
         client_socket_array << create_and_accept_client
@@ -55,17 +42,19 @@ describe 'GameInterface' do
     end
 
     it 'sends the private result description to the player who took the turn' do
-      clients_and_sockets = create_client_and_socket_array(3)
+      clients_and_sockets = create_clients_and_sockets(3)
       interface = GameInterface.new([clients_and_sockets[0][:socket], clients_and_sockets[1][:socket], clients_and_sockets[2][:socket]])
-      result = TurnResult.new(interface.clients_to_players[0][:player])
+      turn_player = interface.player_interfaces[0].player
+      result = TurnResult.new(turn_player)
       interface.describe_result_to_clients(result)
       expect(clients_and_sockets[0][:client].gets.chomp).to eq result.private_description
     end
 
     it 'sends the public result description to all players who did not take the turn' do
-      clients_and_sockets = create_client_and_socket_array(3)
+      clients_and_sockets = create_clients_and_sockets(3)
       interface = GameInterface.new([clients_and_sockets[0][:socket], clients_and_sockets[1][:socket], clients_and_sockets[2][:socket]])
-      result = TurnResult.new(interface.clients_to_players[0][:player])
+      turn_player = interface.player_interfaces[0].player
+      result = TurnResult.new(turn_player)
       interface.describe_result_to_clients(result)
       expect(clients_and_sockets[1][:client].gets.chomp).to eq result.public_description
       expect(clients_and_sockets[2][:client].gets.chomp).to eq result.public_description
