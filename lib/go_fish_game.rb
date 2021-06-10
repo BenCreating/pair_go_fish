e3c41a157d9758504280f68b9794baa32658e4e8
Ben Smith <ben@smithsrus.com>
2021-06-10 13:34:42 -0400

    add #deal_starting_cards to game class

--- lib/go_fish_game.rb
+++ lib/go_fish_game.rb
@@ -1,10 +1,22 @@
+require 'shuffling_deck'
+
 class GoFishGame
   attr_reader :players, :deck
 
   CARDS_NEEDED_FOR_A_SET = 4
+  STARTING_CARD_COUNT = 5
 
-  def initialize(players, deck = 'deck')
+  def initialize(players, deck = ShufflingDeck.new)
     @players = players
     @deck = deck
+  end
+
+  def deal_starting_cards
+    players.each do |player|
+      STARTING_CARD_COUNT.times do
+        card = deck.deal
+        player.take_card(card)
+      end
+    end
   end
 end
