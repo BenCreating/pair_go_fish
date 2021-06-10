e05e4be2f656e8d9248eec45550f18bd23040a8e
Ben Smith <ben@smithsrus.com>
2021-06-10 13:02:43 -0400

    player holds a hand of cards

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,8 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :hand
 
-  def initialize(name = 'Alfred')
+  def initialize(name = 'Alfred', hand = PlayerHand.new([]))
     @name = name
+    @hand = hand
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,4 +1,6 @@
 require_relative '../lib/player'
+require_relative '../lib/player_hand'
+require_relative '../lib/playing_card'
 
 describe 'Player' do
   context 'initialize' do
@@ -10,6 +12,12 @@ describe 'Player' do
     it 'reverts to a default name if none is supplied' do
       player = Player.new
       expect(player.name).to_not eq nil
+    end
+
+    it 'allows passing in a hand of cards' do
+      hand = PlayerHand.new([PlayingCard.new('A'), PlayingCard.new('3')])
+      player = Player.new('Fred', hand)
+      expect(player.hand).to eq hand
     end
   end
 end
