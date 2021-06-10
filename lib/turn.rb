eaecc6be7b1c7eb8174308c643e162351ec4663b
Ben Smith <ben@smithsrus.com>
2021-06-10 14:13:26 -0400

    add game attribute to Turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,8 @@
 class Turn
-  attr_reader :turn_player
-  
-  def initialize(player)
+  attr_reader :turn_player, :game
+
+  def initialize(player, game)
     @turn_player = player
+    @game = game
   end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,10 +1,16 @@
 require_relative '../lib/turn'
 require_relative '../lib/player'
 
+class MockGoFishGame
+
+end
+
 describe 'Turn' do
+  let(:game) { MockGoFishGame.new }
+
   it 'turn is associated with a player' do
     player = Player.new
-    turn = Turn.new(player)
+    turn = Turn.new(player, game)
     expect(turn.turn_player).to eq player
   end
 end
