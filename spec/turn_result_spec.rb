441fa12d4bdfd13f5b697f107b6d369bf44f3c5b
Ben Smith <ben@smithsrus.com>
2021-06-11 19:26:30 -0400

    refactor turn result spec

--- spec/turn_result_spec.rb
+++ spec/turn_result_spec.rb
@@ -1,18 +1,21 @@
 require_relative '../lib/turn_result'
 require_relative '../lib/player'
+require_relative '../lib/playing_card'
 
 describe 'TurnResult' do
+  let(:player_1) { Player.new('Player 1') }
+  let(:player_2) { Player.new('Player 2') }
+  let(:turn_result) { turn_result = TurnResult.new(player_1, player_2) }
+
   context '#initialize' do
     it 'stores the player who took the turn, and the player they asked for a card' do
-      turn_result = TurnResult.new('player', 'other player')
-      expect(turn_result.turn_player).to eq 'player'
-      expect(turn_result.asked_player).to eq 'other player'
+      expect(turn_result.turn_player).to eq player_1
+      expect(turn_result.asked_player).to eq player_2
     end
   end
 
   context '#public_description' do
     it 'generates a description of the turn that is available to all players' do
-      turn_result = TurnResult.new('player', 'other player')
       description = turn_result.public_description
       expect(description).to eq 'Henry asks Margaret for a 5. Margaret has 0. Henry goes fishing.'
     end
@@ -20,7 +23,6 @@ describe 'TurnResult' do
 
   context '#private_description' do
     it 'generates a description of the turn that is available to only the player who\'s turn it is' do
-      turn_result = TurnResult.new('player', 'other player')
       description = turn_result.private_description
       expect(description).to eq 'You ask Margaret for a 5. Margaret has 0. You go fishing and catch a 7.'
     end
