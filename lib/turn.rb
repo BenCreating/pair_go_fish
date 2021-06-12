fe2aa6457c9deefab196bfbd187932c5a64e4d84
Ben Smith <ben@smithsrus.com>
2021-06-12 19:22:07 -0400

    refactor #play

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,16 +16,37 @@ class Turn
 
   def play
     turn_player.take_card(game.fish_for_card) if turn_player.cards_left == 0
-    asked_card = pick_a_card_to_ask_for
-    asked_player = pick_a_player_to_ask
-    taken_cards = asked_player.give_cards_by_rank(asked_card.rank)
-    taken_cards = [fish_for_card] if taken_cards.empty?
-    taken_cards.each { |card| turn_player.take_card(card) }
-    TurnResult.new(turn_player, asked_player, asked_card: asked_card, taken_cards: taken_cards, fished_for_card: false, completed_set: false)
+    taken_cards = ask_for_card
+    taken_cards = go_fish_if_necessary(taken_cards)
+    pick_up_taken_cards(taken_cards)
+    TurnResult.new(turn_player, asked_player, asked_card: asked_card, taken_cards: taken_cards, fished_for_card: fished_for_card, completed_set: false)
   end
 
   private
+
+  attr_accessor :asked_card, :asked_player, :fished_for_card
+
   def fish_for_card
     game.fish_for_card
+  end
+
+  def ask_for_card
+    self.asked_card = pick_a_card_to_ask_for
+    self.asked_player = pick_a_player_to_ask
+    asked_player.give_cards_by_rank(asked_card.rank)
+  end
+
+  def go_fish_if_necessary(taken_cards)
+    if taken_cards.empty?
+      taken_cards = [fish_for_card]
+      self.fished_for_card = true
+    else
+      self.fished_for_card = false
+    end
+    taken_cards
+  end
+
+  def pick_up_taken_cards(taken_cards)
+    taken_cards.each { |card| turn_player.take_card(card) }
   end
 end
