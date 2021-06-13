f66a05a59a896c86e25b32afda122022b0581333
Ben Smith <ben@smithsrus.com>
2021-06-13 19:23:56 -0400

    refactor player interface creation

--- lib/game_interface.rb
+++ lib/game_interface.rb
@@ -7,12 +7,8 @@ class GameInterface
   attr_reader :player_interfaces
 
   def initialize(clients, interface_overrides = nil)
-    @player_interfaces = []
-    if !interface_overrides
-      clients.each { |client| player_interfaces << PlayerInterface.new(client) }
-    else
-      clients.each.with_index { |client, index| player_interfaces << interface_overrides[index] }
-    end
+    @player_interfaces = new_player_interfaces_or_overrides(interface_overrides)
+    @player_interfaces = new_player_interfaces_or_overrides(clients, interface_overrides)
   end
 
   def describe_result_to_clients(result)
@@ -28,5 +24,17 @@ class GameInterface
   def pass_question_to_player(player, all_players, question)
     target_player_interface = (player_interfaces.select { |interface| interface.player == player }).pop
     target_player_interface.ask_a_question_and_wait_for_response(question, all_players)
+  end
+
+  private
+
+  def new_player_interfaces_or_overrides(clients, interface_overrides)
+    interface_array = []
+    if !interface_overrides
+      clients.each { |client| interface_array << PlayerInterface.new(client) }
+    else
+      clients.each.with_index { |client, index| interface_array << interface_overrides[index] }
+    end
+    interface_array
   end
 end
